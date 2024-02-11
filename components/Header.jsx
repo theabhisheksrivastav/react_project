d4f63c015930a9badae9bf68db1b07dfbde2c549
abhisheksrivastavofficial <abhisheksrivastavofficial@gmail.com>
2024-02-11 19:33:18 +0530

    test2

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+import { useState, useCallback, useEffect, useId } from "react";
+import { Link } from "react-router-dom";
+
+function Header() { 
+    
+    return (
+        <>
+            <h1 className= "text-3xl font-bold underline">Welcome to the ap!</h1>
+        </>
+    )
+}
+
+
+export default Header;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from '../components/Header.jsx'
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to the app!</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
